db5762a26d1acddefdde2107f8424564ee583d75
Sam-programs <130783534+Sam-programs@users.noreply.github.com>
2024-02-29 08:49:44 +0300

    fix: regex missing a < ctrl-d appearing inccorectly

--- lua/keys.lua
+++ lua/keys.lua
@@ -5,27 +5,49 @@ local ns = vim.api.nvim_create_namespace("keys")
 local plugin_loaded = false
 local config = {
     win_opts = {
-        relative="editor", style="minimal", border="shadow",
-        row=vim.o.lines, col=vim.o.columns, width=25, height=3
+        relative = "editor",
+        style = "minimal",
+        border = "shadow",
+        row = vim.o.lines,
+        col = vim.o.columns,
+        width = 25,
+        height = 3
     },
     enable_on_startup = false
 }
 local win_loaded = false
 
-local k = vim.keycode or function(str)
-    return vim.api.nvim_replace_termcodes(str,true,true,true)
+local t = vim.keycode or function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 -- TODO: ⇧+ (for shift+), ⌥+ (for alt+) (also see: https://wincent.com/wiki/Unicode_representations_of_modifier_keys)
 local spec_table = {
-    [k "<tab>"] = "⇥ ", [k "<cr>"] = "⏎ ", [k "<esc>"] = "⎋", [" "] = "␣",
-    [k "<del>"] = "⌦", [k "<bs>"] = "⌫",
-    [k "<up>"] = "↑", [k "<down>"] = "↓",[k "<left>"] = "←", [k "<right>"] = "→",
-    [k "<home>"] = "⇱", [k "<end>"] = "⇲",[k "<PageUp>"] = "⇞", [k "<PageDown>"] = "⇟",
+    [t "<tab>"] = "⇥",
+    [t "<cr>"] = "⏎ ",
+    [t "<esc>"] = "⎋",
+    [" "] = "␣",
+    [t '<S-Space>'] = '⇧󱁐',
+    [t "<del>"] = "⌦",
+    [t "<bs>"] = "⌫",
+    [t "<up>"] = "↑",
+    [t "<down>"] = "↓",
+    [t "<left>"] = "←",
+    [t "<right>"] = "→",
+    [t "<home>"] = "⇱",
+    [t "<end>"] = "⇲",
+    [t "<PageUp>"] = "⇞",
+    [t "<PageDown>"] = "⇟",
+    [t "<insert>"] = "⎀",
+    [t "<C-d>"] = "⌃d",
 }
+
 local spc = {
-    ["<t_\253g>"] = " ", -- lua function
-    ["<Cmd>"] = "",
+    ["<t_\253g>"] = " ", -- lua function key
+    -- Sometimes keytrans incorrectly translates keys to <C-D>.
+    -- Ctrl-d is handled in spec_table instead
+    ["<C-D>"] = false,
+    ["<Cmd>"] = false,
 }
 
 --- Get the last 5 keys
@@ -36,22 +58,21 @@ M.current_keys = function(as_string)
 end
 
 local create_float = function()
-	buf = vim.api.nvim_create_buf(false, true)
-	win = vim.api.nvim_open_win(buf, false, config.win_opts)
-	-- vim.api.nvim_win_set_option(win, "winhighlight", "Normal:KeysNormal")
-	vim.api.nvim_buf_set_option(buf, "filetype", "keys")
+    buf = vim.api.nvim_create_buf(false, true)
+    win = vim.api.nvim_open_win(buf, false, config.win_opts)
+    -- vim.api.nvim_win_set_option(win, "winhighlight", "Normal:KeysNormal")
+    vim.api.nvim_buf_set_option(buf, "filetype", "keys")
 end
 
 local render = function()
     local text = table.concat(typed_letters, ' ')
-    local pad = (" "):rep(math.floor((config.win_opts.width-vim.api.nvim_strwidth(text))/2))
-	local set_lines = pad .. table.concat(typed_letters, " ") .. pad
-    vim.api.nvim_buf_set_lines(buf, 1, 2, false, {set_lines})
+    local pad = (" "):rep(math.floor((config.win_opts.width - vim.api.nvim_strwidth(text)) / 2))
+    local set_lines = pad .. table.concat(typed_letters, " ") .. pad
+    vim.api.nvim_buf_set_lines(buf, 1, 2, false, { set_lines })
 end
 
--- local t = function(k) return vim.api.nvim_replace_termcodes(k, true, true, true) end
 local sanitize_key = function(key)
-    for k,v in pairs(spec_table) do
+    for k, v in pairs(spec_table) do
         if key == k then
             return v
         end
@@ -62,17 +83,17 @@ local sanitize_key = function(key)
     end
 
     local translated = vim.fn.keytrans(key)
-    local match = translated:match("^C.-(.)>$")
+    local special = spc[translated]
+    if special ~= nil then
+        return special
+    end
+    local match = translated:match("^<C.-(.)>$")
     if match then
         local shift = translated:match("^<C[-]S[-].>$")
         if not shift then
             match = match:lower()
         end
-        return  '⌃' .. match
-    end
-    local special = spc[translated]
-    if special ~= nil then
-        return special
+        return '⌃' .. match
     end
 
     -- Mouse events
@@ -105,13 +126,13 @@ end
 
 M.setup = function(opt)
     config = vim.tbl_deep_extend("force", config, opt or {})
-    if config.enable_on_startup then start_registering() end
+    if config.enable_on_startup then M.toggle() end
 end
 
 M.toggle = function()
     if win_loaded then
         vim.api.nvim_win_close(win, true)
-        vim.api.nvim_buf_delete(buf, {force=true})
+        vim.api.nvim_buf_delete(buf, { force = true })
         vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
     else
         create_float()
